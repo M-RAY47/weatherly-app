9e467312ce5d11bad88c156bf2b787849d88e7c4
M.RAY <a.mray47@yahoo.fr>
2021-09-30 16:02:04 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const app = express();
+
+const apikey = "";
+app.use((req, res, next)=>{
+	let {method, path, ip} = req;
+	let str =req.method + " " + req.path + " - " + req.ip;
+	console.log(str);
+	next();
+})
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res){
+	res.render("index", {weather: null, error : null});
+})
+
+app.listen(3010, function (){
+	console.log("Weatherly app is listening on port 3010!");
+})
