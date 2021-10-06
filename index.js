f132c5149827d98d84fee69354cc699effdc0460
M.RAY <a.mray47@yahoo.fr>
2021-10-06 21:31:07 +0800

    Add comment

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use((req, res, next)=>{
 })
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+// setting the app on ejs
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
